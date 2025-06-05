178742468fa8359ca2761bc9816d5edd2a77497f
SPapale <santiagopapale08@gmail.com>
2025-06-05 09:56:43 -0300

    SUbo ejericio 1 complet

--- Ejercicio_1/src/PrimeraClase.java
+++ Ejercicio_1/src/PrimeraClase.java
@@ -1,13 +1,9 @@
-import java.util.Scanner;
+
 
 public class PrimeraClase {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
 		for(int i = 1; i < 4; i++) {
-			System.out.print("Ingrese un numero ");
-			i = scanner.nextInt();
 			if(i % 2 == 0) {
 				System.out.println("Par ");
 			}
@@ -15,7 +11,6 @@ public class PrimeraClase {
 				System.out.println(+ i);
 			}
 		}
-		scanner.close();
 	}
 
 }
--- Ejercicio_2/src/PrimeraClase.java
+++ Ejercicio_2/src/PrimeraClase.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class PrimeraClase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		int i, divisores=0, numero=0;
+		for(i=1; i<=numero; i++) {
+			System.out.println("Ingrese un numero ");
+			numero = scanner.nextInt();
+			if(numero % i == 0) {
+				divisores++;
+			}
+			
 	}
 
+}
+	
 }
