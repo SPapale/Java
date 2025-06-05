582f14130f2cef20cf16a83b29efa0f16f96507b
SPapale <santiagopapale08@gmail.com>
2025-06-05 09:25:01 -0300

    Subo ejercicio 1 modificado mas base del 2

--- Ejercicio_1/src/PrimeraClase.java
+++ Ejercicio_1/src/PrimeraClase.java
@@ -4,15 +4,15 @@ public class PrimeraClase {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int numero;
-		for(int i = 1; i <= 3; i++) {
+		
+		for(int i = 1; i < 4; i++) {
 			System.out.print("Ingrese un numero ");
-			numero = scanner.nextInt();
-			if(numero % 2 == 0) {
+			i = scanner.nextInt();
+			if(i % 2 == 0) {
 				System.out.println("Par ");
 			}
 			else {
-				System.out.println(+ numero);
+				System.out.println(+ i);
 			}
 		}
 		scanner.close();
