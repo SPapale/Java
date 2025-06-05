bf52755501d78212d8fe7cb98d2186fa94c61ec9
SPapale <santiagopapale08@gmail.com>
2025-06-05 14:40:51 -0300

    Subo ejercicio 2

--- Ejercicio_2/src/PrimeraClase.java
+++ Ejercicio_2/src/PrimeraClase.java
@@ -1,18 +1,29 @@
 
-
 public class PrimeraClase {
 
 	public static void main(String[] args) {
-		int i, divisores=0;
-		for(i=50; i<=100; i++) {
-			if(i % 2 > 0) {
-				divisores++;
+		for(int i=50; i<=100; i++) {
+			int divisores=0;
+			
+			for(int j=1; j<= i; j++) {
+				if(i % j ==  0) {
+					divisores++;
+				}
+			}
+			if(divisores == 2) {
+				System.out.println("\n" + i + ": es primo");
+			}
+			else {
+				System.out.println("\n" + i + ":  divisores    ");
+				for (int j = 1; j <= i; j++) {
+                    if (i % j == 0) {
+                        System.out.print(j + " ");
+                    }
+                }
+				
 			}
 		}
-		if(divisores == 2) {
-			System.out.println("Es par");
-		}
 
-}
-	
+	}
+
 }
