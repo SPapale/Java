79a9bb8c866788e09c43251936ed2393e2c26b66
SPapale <santiagopapale08@gmail.com>
2025-06-11 17:27:48 -0300

    Subo ejercicio 24

---
+++ Ejercicio_24/src/ClasePrimaria.java
@@ -0,0 +1,15 @@
+
+public class ClasePrimaria {
+
+	public static void main(String[] args) {
+		// Hacemos un array con nombres de personas
+        String[] nombres = {"Ana", "Luis", "Pedro", "Sofía"};
+
+        // Recorremos con for each y mostramos cada nombre
+        for (String nombre : nombres) {
+            System.out.println("Nombre: " + nombre);
+        }
+
+	}
+
+}
