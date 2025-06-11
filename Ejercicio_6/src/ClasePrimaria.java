1301bc17cce2aaa282e61aacab18eaec49afec34
SPapale <santiagopapale08@gmail.com>
2025-06-11 15:51:44 -0300

    Subo ejercicio 6

--- Ejercicio_6/src/ClasePrimaria.java
+++ Ejercicio_6/src/ClasePrimaria.java
@@ -1,9 +1,20 @@
-
+import java.util.Scanner; 
 public class ClasePrimaria {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in); 
+		
+		System.out.println("Ingrese una palabra o frase: ");
+		
+		String frase = scanner.nextLine(); 
+		
+		String invertido = ""; 
+		
+		for(int i = frase.length() - 1; i >=0; i--) {
+			invertido = invertido + frase.charAt(i); 
+		}
+		
+		System.out.println("Palabra o frase invertida: " + invertido); 
 	}
 
 }
